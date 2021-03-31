f74a771408421848fc0e3fdf5a3771a48e64962b
RobinD95 <lopfi95@gmail.com>
2021-04-01 00:29:21 +0200

    port over to platformio

--- temp-humid-watchdog/Debug/ESPAsyncTCP-master/tcp_axtls.c.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Robin\AppData\Local\Temp\VMBuilds\temp-humid-watchdog\esp8266_d1_mini\Debug\ESPAsyncTCP-master\tcp_axtls.c.o: \
- C:\Users\Robin\Documents\Arduino\libraries\ESPAsyncTCP-master\src\tcp_axtls.c \
- C:\Users\Robin\Documents\Arduino\libraries\ESPAsyncTCP-master\src/async_config.h \
- C:\Users\Robin\Documents\Arduino\libraries\ESPAsyncTCP-master\src/DebugPrintMacros.h
